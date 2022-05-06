627920f2ad829b4f8d345a1da4da4140a3c1ddd8
sakaria <sakariafarah@yahoo.com>
2022-05-05 22:20:41 -0500

    commit tree bfbfbbf8b7c90053bf17511ae4b58b77a2d2cba2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity='roller-skate'){
+    return (`This Saturday, I want to ${activity}!`)
+
+    }
+    saturdayFun()
+    function mondayWork(location='go to the office'){
+        return (`This Monday, I will ${location}.`)
+    }
+    mondayWork()
+    function wrapAdjective(flare='*'){
+return function (word='special'){
+    return(`You are ${flare}${word}${flare}!`)
+    
+
+}
+
+    }
